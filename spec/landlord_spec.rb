102b75c3c32420ec4de154847f78bfb577c8b638
Tyrone Williams <tyrone@Tyrones-MacBook-Air.local>
2022-01-17 17:38:32 +0000

    landlord_spec

---
+++ spec/landlord_spec.rb
@@ -0,0 +1,8 @@
+describe 'landlord' do
+    describe '.add' do
+        it 'should add property' do
+            property = Landlord.add("property_1")
+            expect(property).to eq "property_1"
+        end
+    end
+end
