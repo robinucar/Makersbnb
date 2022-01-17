e41b791fa9931534af56c19b2ecf15079a4e08ee
Jason Humbert <jasonhumbert@outlook.com>
2022-01-17 16:31:43 +0000

    Add spaces index.erb

--- spec/features/add_space_spec.rb
+++ spec/features/add_space_spec.rb
@@ -1,7 +1,7 @@
 feature 'add spaces' do
   scenario 'landlord can list empty spaces' do
     visit('/spaces')
-    fill_in('spaces', with: 'Space 1')
+    fill_in('space', with: 'Space 1')
     click_button('Add')
 
     expect(page).to have_content 'Space 1'
