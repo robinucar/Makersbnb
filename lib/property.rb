930b34a5d384c67c43f681c8f298e5bd130e1941
Tyrone Williams <tyrone@Tyrones-MacBook-Air.local>
2022-01-18 14:34:31 +0000

    US3 WIP

--- lib/property.rb
+++ lib/property.rb
@@ -1,7 +1,13 @@
 require 'pg'
 
 class Property
-  attr_reader :properties
+  attr_reader :properties, :id, :description, :price
+
+  def initialize(id:, description:, price:)
+    @id = id
+    @description = description
+    @price = price
+  end
 
   def self.add(property:)
     if ENV['ENVIRONMENT'] == 'test'
@@ -10,9 +16,9 @@ class Property
       connection = PG.connect dbname: 'bnb_app'
     end
 
-    properties = connection.exec_params("INSERT INTO properties (description) VALUES ($1) RETURNING description", [property])
-
-    properties
+    property = connection.exec_params("INSERT INTO properties (description) VALUES ($1) RETURNING description", [property])
+    Property.new(id: property[0]["id"], description: property[0]["description"], price: property[0]["price"])
+    # properties
 	end
 
   def self.all
@@ -25,7 +31,7 @@ class Property
     properties = connection.exec_params("SELECT * FROM properties;")
 
     #p 'properties.map: ', 
-    properties.map { |property| property['description'] }
+    properties.map { |property| Property.new(id: property[0]["id"], description: property[0]["description"], price: property[0]["price"]) }
     # ["property_1", "property_2"]
   end
 end
